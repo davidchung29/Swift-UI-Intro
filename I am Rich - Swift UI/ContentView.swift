44e2545e5b8fd960193073f8f665dcd086bf4f26
davidchung29 <60008214+dvjr2018@users.noreply.github.com>
2020-12-28 10:55:58 -0800

    finished intro project

--- I am Rich - Swift UI/ContentView.swift
+++ I am Rich - Swift UI/ContentView.swift
@@ -4,18 +4,34 @@
 //
 //  Created by David Jr on 12/28/20.
 //
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(.systemTeal).edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("I am rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            
+        }
     }
 }
+
+
