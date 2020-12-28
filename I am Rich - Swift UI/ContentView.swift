bae73239fb0fb02a56ecea0f760b1d1fa6df51ee
davidchung29 <60008214+dvjr2018@users.noreply.github.com>
2020-12-28 10:19:49 -0800

    Initial Commit

---
+++ I am Rich - Swift UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I am Rich - Swift UI
+//
+//  Created by David Jr on 12/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
